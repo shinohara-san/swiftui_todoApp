24152550fbb5496902fce7b9e9368ad63a38afec
shinohara-san <someiyoshino0809@gmail.com>
2020-03-17 13:08:48 +0900

    Initial Commit

---
+++ 02todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  02todo
+//
+//  Created by Yuki Shinohara on 2020/03/17.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
